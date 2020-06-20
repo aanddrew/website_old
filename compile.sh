01c38701132d155e7ec9debdc3d344bed2d15392
Andrew Weller <andrewweller.cs@gmail.com>
2020-06-20 06:37:20 +0000

    Improve compile script, using awk instead of echo

--- compile.sh
+++ compile.sh
@@ -2,17 +2,7 @@
 
 INDEX="About/main.html"
 
-function insertIntoTemplate {
-    BARLINE=$(grep -n BARGOESHERE template.html | cut -d ':' -f 1)
-    TEXTLINE=$(grep -n TEXTGOESHERE template.html | cut -d ':' -f 1)
-
-    TOP="$(sed -n 1,$(expr ${BARLINE} - 1)p template.html)"
-    MIDDLE="$(sed -n $(expr ${BARLINE} + 1),$(expr ${TEXTLINE} - 1)p template.html)"
-    BOT="$(sed -n $(expr ${TEXTLINE} + 1),$(wc -l template.html | cut -d ' ' -f 1)p template.html)"
-
-    echo -e "$TOP\n $BAR $MIDDLE <br> $1\n $2 \n $BOT"
-}
-
+#used to figure out if the file should be the index or not 
 function getOfileName {
     if [ $1 = $INDEX ]  
     then
@@ -22,6 +12,7 @@ function getOfileName {
     fi
 }
 
+#build bar before we do anything else
 BAR="<ul class=navbar>"
 for content in */main.html ; do
     name=${content%/*}
@@ -29,7 +20,16 @@ for content in */main.html ; do
 done
 BAR="${BAR}</ul>"
 
+#must set the variables $BAR $TEXT $EXTRA before using this function
+function insertIntoTemplate {
+    awk -v bar="$BAR" -v text="$TEXT" -v extra="$EXTRA" \
+        '{gsub("BARGOESHERE", bar); gsub("TEXTGOESHERE", text); gsub("EXTRAGOESHERE", extra); print}' template.html
+}
+
 for content in */main.html ; do
+
+    #This extra depends on the page, right now it is used to 
+    #dynamically build the list of projects on the projects page
     EXTRA=""
     if [ $content = "Projects/main.html" ]
     then
@@ -37,17 +37,18 @@ for content in */main.html ; do
         OIFS="$IFS"
         IFS=$'\n'
         for project in $(ls Projects | grep -v main); do
-            EXTRA="$EXTRA <li> <a href=\"$project\">$(sed 's/_/ /g' <(echo ${project%.html}))</a> </li>"
-            echo $(insertIntoTemplate "$(cat Projects/$project)") > build/${project}
+            TEXT="$(cat Projects/$project)"
+            EXTRA=`printf "%s\n    %s" "$EXTRA" "<li> <a href=\"$project\">$(sed 's/_/ /g' <(echo ${project%.html}))</a> </li>"`
+            insertIntoTemplate > build/${project}
         done
         IFS=$OIFS
-        EXTRA="$EXTRA </ul>"
+        EXTRA=`printf "%s\n%s" "$EXTRA" "</ul>"`
     fi
 
     OFILE="$(getOfileName $content)"
-
     mkdir -p build
-    echo $(insertIntoTemplate "$(cat $content)" "$EXTRA") > build/${OFILE}
+    TEXT="$(cat $content)"
+    insertIntoTemplate > build/${OFILE}
 done
 
 cp styles.css build/styles.css
